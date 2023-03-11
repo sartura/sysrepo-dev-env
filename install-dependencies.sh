fe7adcec49a847ffd145f079672e98dabadfe479
Mateo Cindric <mateocindricdev@gmail.com>
2023-03-11 15:32:47 +0000

    Add clang as dependency

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-apt install doxygen valgrind cmake libcmocka-dev libsystemd-dev libpcre3-dev libpcre2-dev bison flex pkg-config graphviz shunit2
+apt install doxygen valgrind cmake libcmocka-dev libsystemd-dev libpcre3-dev libpcre2-dev bison flex pkg-config graphviz shunit2 clang
